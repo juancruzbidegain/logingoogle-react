873fc2985b7589a754dfb3738265cf3181605281
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-03-06 00:53:27 -0300

    components

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import "../styles/Login.css"
+
+const Login = () => {
+    return (
+        <div className="login">
+            <h1>Soy el login</h1>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+import "../styles/Post.css"
+
+const Post = () => {
+    return (
+        <div className="post">
+               <h1>Post</h1>
+        </div>
+    )
+}
+
+export default Post
